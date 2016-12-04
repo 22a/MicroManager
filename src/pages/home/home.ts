9a2c67be1372e69c39bf35d87bf0d16e2c7a32af
22a <meehanp2@tcd.ie>
2016-12-04 15:14:03 +0000

    made homescreen great again :art:

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+// import { Today } from '../today/Today';
+import { Leaderboard } from '../leaderboard/Leaderboard';
+// import { Meals } from '../meals/Meals';
+import { Record } from '../record/Record';
 
 @Component({
   selector: 'home',
@@ -9,6 +13,21 @@ import { NavController } from 'ionic-angular';
 export class Home {
 
   constructor(public navCtrl: NavController) {
+  }
+
+  goToToday(){
+    // this.navCtrl.push(Today);
+    console.log("today page doesn't exist yet, when it does uncomment the above");
+  }
+  goToLeaderboard(){
+    this.navCtrl.push(Leaderboard);
+  }
+  goToMeals(){
+    // this.navCtrl.push(Meals);
+    console.log("meal page doesn't exist yet, when it does uncomment the above");
+  }
+  goToRecord(){
+    this.navCtrl.push(Record);
   }
 
 }

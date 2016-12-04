6b63ebd994b3867b338a320c31449d56f301a774
shawa <andrewshaw.mail@gmail.com>
2016-12-04 12:58:28 +0000

    Module skeleton added

---
+++ src/pages/signup/signup.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'signup',
+  templateUrl: 'signup.html'
+})
+export class Signup{
+
+  constructor(public navCtrl: NavController) {
+  }
+
+}

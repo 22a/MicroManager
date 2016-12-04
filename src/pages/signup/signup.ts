b4be5b76d8954de6cf7e5a77ff3ff11dbcb1fa2f
22a <meehanp2@tcd.ie>
2016-12-04 19:40:51 +0000

    beautiful signup, added upload dialog + signup nav

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, ActionSheetController } from 'ionic-angular';
+import { Home } from '../home/home';
 
 @Component({
   selector: 'signup',
@@ -8,7 +9,38 @@ import { NavController } from 'ionic-angular';
 })
 export class Signup{
 
-  constructor(public navCtrl: NavController) {
+  measurement = "metric";
+
+  constructor(public navCtrl: NavController, public actionSheetCtrl: ActionSheetController) {
   }
 
+  goToHome() {
+    this.navCtrl.setRoot(Home);
+  }
+
+  presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Upload Profile Picture',
+      buttons: [
+        {
+          text: 'Take Photo',
+          handler: () => {
+            console.log('the guzzler wants to take a new photo, wowee, who cares');
+          }
+        },{
+          text: 'Choose from Camera Roll',
+          handler: () => {
+            console.log('the gripplem wants to choose a photo they\'ve photoshopped');
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('chickened out innit');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
 }

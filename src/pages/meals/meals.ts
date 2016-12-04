8911df66022d6847dae11afa35ce6abbe1b28e49
c-brenn <brennc27@tcd.ie>
2016-12-04 13:44:26 +0000

    page layout and fake meals :older_woman:

--- src/pages/meals/meals.ts
+++ src/pages/meals/meals.ts
@@ -7,8 +7,40 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'meals.html'
 })
 export class Meals {
+  items;
 
-  constructor(public navCtrl: NavController) {
+  constructor() {
+    this.initializeItems();
   }
 
+  initializeItems() {
+    this.items = [
+      { 'name': 'Vietnamese Pho 0', 'description': 'delicious poop', 'imgName': 'pho.jpg' },
+      { 'name': 'Vietnamese Pho 1', 'description': 'delicious poop', 'imgName': 'pho.jpg' },
+      { 'name': 'Vietnamese Pho 2', 'description': 'delicious poop', 'imgName': 'pho.jpg' },
+      { 'name': 'Vietnamese Pho 3', 'description': 'delicious poop', 'imgName': 'pho.jpg' },
+      { 'name': 'Vietnamese Pho 4', 'description': 'delicious poop', 'imgName': 'pho.jpg' },
+      { 'name': 'Vietnamese Pho 5', 'description': 'delicious poop', 'imgName': 'pho.jpg' },
+      { 'name': 'Vietnamese Pho 6', 'description': 'delicious poop', 'imgName': 'pho.jpg' },
+      { 'name': 'Vietnamese Pho 7', 'description': 'delicious poop', 'imgName': 'pho.jpg' },
+      { 'name': 'Vietnamese Pho 8', 'description': 'delicious poop', 'imgName': 'pho.jpg' },
+      { 'name': 'Vietnamese Pho 9', 'description': 'delicious poop', 'imgName': 'pho.jpg' },
+      { 'name': 'Vietnamese Pho 10', 'description': 'delicious poop', 'imgName': 'pho.jpg' }
+    ];
+  }
+
+  getItems(ev) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the ev target
+    var val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
 }
